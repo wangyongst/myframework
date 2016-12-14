a72cb35b9517eb41d720ac92f2fe1ee5a3db3638
wangyongst <wangyongst@gmail.com>
2016-12-14 14:03:39 +0800

    office

---
+++ java.restful/src/main/java/com/framework/App.java
@@ -0,0 +1,26 @@
+package com.framework;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        String url = "https://eerg.talentlms.com/api/v1/gotocourse/user_id:2,course_id:125,course_completed_redirect:google.com,logout_redirect:google.com";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("Authorization", "Basic WUhQV2VtRzhSZXdMbG0yNFdUTEtNWmFsSEFKcGxiOg==");
+        HttpEntity<?> requestEntity = new HttpEntity(requestHeaders);
+        HttpEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+        String body = response.getBody();
+        JSONObject json = new JSONObject(body);
+        System.out.println(json.getString("goto_url"));
+        String result = restTemplate.getForObject(json.getString("goto_url"), String.class);
+        System.out.println(result);
+    }
+}
