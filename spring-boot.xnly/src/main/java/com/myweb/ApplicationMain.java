dabe5e0a9b2a65d42d5ec7e82d94717c63c14489
wangyongst <wangyongst@gmail.com>
2016-12-21 15:54:09 +0800

    office

--- spring-boot.xnly/src/main/java/com/myweb/ApplicationMain.java
+++ spring-boot.xnly/src/main/java/com/myweb/ApplicationMain.java
@@ -1,11 +1,16 @@
 package com.myweb;
 
+import com.framework.boot.servlet3.WebInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by BHWL on 2016-12-21.
  */
+@SpringBootApplication
 public class ApplicationMain {
     public static void main(String[] args) throws Exception {
         //启动Spring Boot项目的唯一入口
-        //SpringApplication.run(WebInitializer.class, args);
+        SpringApplication.run(WebInitializer.class, args);
     }
 }
