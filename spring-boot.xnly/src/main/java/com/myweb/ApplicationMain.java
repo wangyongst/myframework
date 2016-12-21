1a873622dec07c6d9388a8a04d8f93232b0ffb6a
wangyongst <wangyongst@gmail.com>
2016-12-21 16:43:48 +0800

    office

--- spring-boot.xnly/src/main/java/com/myweb/ApplicationMain.java
+++ spring-boot.xnly/src/main/java/com/myweb/ApplicationMain.java
@@ -2,12 +2,14 @@ package com.myweb;
 
 import com.framework.boot.servlet3.WebInitializer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by BHWL on 2016-12-21.
  */
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ApplicationMain {
     public static void main(String[] args) throws Exception {
         //启动Spring Boot项目的唯一入口
