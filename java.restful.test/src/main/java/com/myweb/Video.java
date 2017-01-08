ad12a6818512e5533d8821305053907c8b8a78da
wangyongst <wangyongst@gmail.com>
2017-01-08 12:59:17 +0800

    home

---
+++ java.restful.test/src/main/java/com/myweb/Video.java
@@ -0,0 +1,29 @@
+package com.myweb;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by BHWL on 2016-12-28.
+ */
+public class Video {
+
+    public static int time = 0;
+
+    public static void main(String[] args) {
+        String url = "http://api.47ks.com/webcloud/?v=";
+        url = url + "http://www.iqiyi.com/v_19rra0dh6c.html?fc=8b62d5327a54411b#vfrm=19-9-0-1";
+        System.out.println(url);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders requestHeaders = new HttpHeaders();
+        HttpEntity<?> requestEntity = new HttpEntity(requestHeaders);
+        HttpEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+        String body = response.getBody();
+        System.out.println(body);
+
+    }
+
+}
