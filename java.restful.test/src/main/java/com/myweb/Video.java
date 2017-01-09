494596472021abc3208e7ba4f62ac6b9d305d683
wangyongst <wangyongst@gmail.com>
2017-01-09 15:46:22 +0800

    office

--- java.restful.test/src/main/java/com/myweb/Video.java
+++ java.restful.test/src/main/java/com/myweb/Video.java
@@ -14,7 +14,8 @@ public class Video {
     public static int time = 0;
 
     public static void main(String[] args) {
-        String url = "http://api.47ks.com/webcloud/?v=";
+        //String url = "http://api.47ks.com/webcloud/?v=";
+        String url = "http://000o.cc/jx/ty.php?url=";
         url = url + "http://www.iqiyi.com/v_19rra0dh6c.html?fc=8b62d5327a54411b#vfrm=19-9-0-1";
         System.out.println(url);
         RestTemplate restTemplate = new RestTemplate();
@@ -23,7 +24,5 @@ public class Video {
         HttpEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
         String body = response.getBody();
         System.out.println(body);
-
     }
-
 }
