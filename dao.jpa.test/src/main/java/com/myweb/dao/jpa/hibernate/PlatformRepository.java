4648bbb58c81f59417443dd84393c78e92ca0b90
wangyongst <wangyongst@gmail.com>
2017-06-01 10:23:53 +0800

    office

--- dao.jpa.test/src/main/java/com/myweb/dao/jpa/hibernate/PlatformRepository.java
+++ dao.jpa.test/src/main/java/com/myweb/dao/jpa/hibernate/PlatformRepository.java
@@ -1,7 +1,5 @@
 package com.myweb.dao.jpa.hibernate;
 
-import com.myweb.pojo.Platform;
-import com.myweb.pojo.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.stereotype.Repository;
--- dao.jpa.test/src/main/java/com/myweb/dao/jpa/hibernate/UserRepository.java
+++ dao.jpa.test/src/main/java/com/myweb/dao/jpa/hibernate/UserRepository.java
@@ -1,6 +1,5 @@
 package com.myweb.dao.jpa.hibernate;
 
-import com.myweb.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.stereotype.Repository;
