0453561cc50c8101668e8cd018e3503c7c67cf3a
wangyongst <wangyongst@gmail.com>
2017-08-15 10:32:37 +0800

    office

--- spring.boot/src/main/java/com/boot/spring/boot/MainApplication.java
+++ spring.boot/src/main/java/com/boot/spring/boot/MainApplication.java
@@ -1,11 +1,19 @@
 package com.boot.spring.boot;
 
+import com.framework.boot.login.authimg.AuthImageServlet;
+import com.framework.boot.login.filter.LoginFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.ArrayList;
-import java.util.EventListener;
 import java.util.List;
 
 /**
@@ -17,13 +25,12 @@ import java.util.List;
 @EntityScan(basePackages = {"com.myweb.pojo"})
 public class MainApplication extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(MainApplication.class,args);
+        SpringApplication.run(MainApplication.class, args);
     }
 
     @Bean
     public FilterRegistrationBean loginFilterRegistrationBean() {
         FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        //LoginFilter
         LoginFilter loginFilter = new LoginFilter();
         registrationBean.setFilter(loginFilter);
         List<String> urlPatterns = new ArrayList<String>();
@@ -33,23 +40,9 @@ public class MainApplication extends SpringBootServletInitializer {
         return registrationBean;
     }
 
-   // @Bean
-    public FilterRegistrationBean hiddenHttpMethodFilterRegistrationBean() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-//
-//        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
-//        registrationBean.setFilter(hiddenHttpMethodFilter);
-//        List<String> urlPatterns = new ArrayList<String>();
-//        urlPatterns.add("*.do");
-//        urlPatterns.add("*.jsp");
-//        registrationBean.set
-        return registrationBean;
-    }
-
     @Bean
     public ServletRegistrationBean authImageServlet() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean();
-
         AuthImageServlet authImageServlet = new AuthImageServlet();
         registrationBean.setServlet(authImageServlet);
         List<String> urlMappings = new ArrayList<String>();
@@ -59,13 +52,13 @@ public class MainApplication extends SpringBootServletInitializer {
     }
 
 
-    //@Bean
-    public ServletListenerRegistrationBean<EventListener> getDemoListener() {
-        ServletListenerRegistrationBean<EventListener> registrationBean = new ServletListenerRegistrationBean<>();
-//        registrationBean.setListener(new Log4jConfigListener());
-//        Map parameters = new HashMap<String, String>();
-//        parameters.put("log4jConfigLocation", "classpath:properties/log4j.properties");
-//        registrationBean.setInitParameters(parameters);
-        return registrationBean;
-    }
+//    //@Bean
+//    public ServletListenerRegistrationBean<EventListener> getDemoListener() {
+//        ServletListenerRegistrationBean<EventListener> registrationBean = new ServletListenerRegistrationBean<>();
+////        registrationBean.setListener(new Log4jConfigListener());
+////        Map parameters = new HashMap<String, String>();
+////        parameters.put("log4jConfigLocation", "classpath:properties/log4j.properties");
+////        registrationBean.setInitParameters(parameters);
+//        return registrationBean;
+//    }
 }
